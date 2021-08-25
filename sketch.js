fac65dd33c6f66853d46bf49855a51cf01cf20c8
Harieshwar-Dilipan <89254307+Harieshwar-Dilipan@users.noreply.github.com>
2021-08-25 07:17:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var ship,travellingShip;
+var sea;seaImage;
+
+function preload(){
+  travellingShip=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+  seaImage=loadImage("sea.png");
+  
+}
+
+function setup(){
+  createCanvas(600,400);
+  sea = createSprite(50,50,200,200);
+  sea.addAnimation("sea",seaImage);
+  sea.x=sea.width/2;
+  sea.velocityX=4;
+  sea.addImage("sea",seaImage);
+
+  ship = createSprite(120,150,100,50);
+  ship.addAnimation("ship",travellingShip);
+  ship.scale=0.2;
+
+
+}
+
+function draw() {
+  background("lightblue");
+console.log(sea.x);
+  if(sea.x<2000){
+    sea.x=sea.legnth/2;
+  }
+
+  drawSprites();
+ 
+}
